9dd0a5131bfdf0fc6cd61d731be9c86fff5b06cd
Daniel Slade <dangslade@gmail.com>
2022-12-23 19:54:22 -0800

    shifts messaging responsibilities from embedmsg to commands, adds lcUtils

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
-
 from lastACs import *
 from getQuestion import *
 from registerPlayer import *
-from unregisterPlayer import *
-from embedMsg import *
 from setupPlayer import *
+from lcUtils import *
 import discord
 from discord.ext import commands
 from os.path import exists
@@ -14,7 +11,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='-', help_command=None, intents=intents)  # sets prefix, deletes default help command, and sets intents
 
-botTitle = "LeetCode Bot"
 
 
 @client.event
@@ -25,14 +21,9 @@ async def on_ready():
     if not exists("players"):
         os.mkdir("players")
 
-@client.before_invoke
-async def common(ctx):
-    await setUpPlayer(ctx)
-
-
 @client.command()
 async def help(ctx):                            # shows the user what commands the bot has
-    embed = discord.Embed(title=botTitle)
+    embed = getEmbed()
     with open("help.txt", "r") as readFile:     # help text is stored in help.txt
         helpText = readFile.read()              # read text then return in an embedded message
     embed.description = helpText
@@ -49,39 +40,11 @@ async def lastSolved(ctx, user, amount=1):      # gets "amount" last questions u
 async def getQuestionWithID(ctx, questionID):   # gets LeetCode question with given ID
     await showQuestion(ctx, questionID)
 
+
 @client.command()
 async def register(ctx, leetCodeName):
-    if getPlayerLCName(ctx) == "null":
-        await registerPlayer(ctx, leetCodeName)    # register LeetCode name for player
-    elif getPlayerLCName(ctx) != "null" or getPlayerLCName(ctx) != leetCodeName:
-        await changeNameEmbed(ctx, leetCodeName)
+    await handleRegister(ctx, leetCodeName, client)
 
-        try:
-            msg = await client.wait_for("message", timeout=15)
-        except asyncio.TimeoutError:                           
-            await timeoutEmbed(ctx)             
-            return
-
-        if msg.content == "Y" or msg.content == "y": # set the new name for the player
-            await registerPlayer(ctx, leetCodeName)
-        else:
-            await changeNameDeclineEmbed(ctx, leetCodeName)      
-    
-
-@client.command()
-async def unregister(ctx):
-  await unregisteringEmbed(ctx)
-  try:
-      msg = await client.wait_for("message", timeout=15)
-  except asyncio.TimeoutError:                           
-      await timeoutEmbed(ctx)          
-      return
-
-  if msg.content == "Y" or msg.content == "y": 
-      await unregisterPlayer(ctx)               # Unregister player
-      await unregisteredEmbed(ctx)
-  else: 
-      pass
 
 with open("key.txt", "r") as readFile:          # get bot token and run
     bot_token = readFile.readline()
--- registerPlayer.py
+++ registerPlayer.py
@@ -1,26 +1,74 @@
 import os
+from urllib.request import urlopen
+
 import discord
 import json
 from os.path import exists
 import asyncio
 from discord.ext import commands
-from embedMsg import *
+from lcUtils import *
+from setupPlayer import *
 
-async def registerPlayer(ctx, leetCodeName): 
-    playerID = str(ctx.author.id)
 
-    with open(f"./players/{playerID}.json") as readFile:
-        playerInfo = json.load(readFile)
+async def handleRegister(ctx, leetCodeName, client):
+    playerInfo = getPlayer(ctx.author.id)
+    embed = getEmbed()
+    if not checkIfAccExists(leetCodeName):
+        embed.description = f"{leetCodeName} is not a valid LeetCode account! Make sure that you are using the name of your LeetCode account."
+        embed.colour = discord.Colour.red()
+        await ctx.send(embed=embed)
+        return
+    if playerInfo is None:
+        registerPlayer(ctx, leetCodeName)  # register LeetCode name for player
+        embed.description = f"You have successfully registered your LeetCode account as `{leetCodeName}`\n Happy LeetCoding, {ctx.author.name}!"
+        embed.colour = discord.Colour.green()
+        await ctx.send(embed=embed)
+        return
+    if playerInfo["LEETCODENAME"] != leetCodeName:
+        embed.description = f"Do you want to reregister your LeetCode account as `{leetCodeName}`? WARNING: THIS WILL RESET YOUR DATA! [Y/N]"
+        embed.colour = discord.Colour.purple()
+        await ctx.send(embed=embed)
 
+        def check(m):
+            return m.author == ctx.author
+        try:
+            msg = await client.wait_for("message", timeout=15, check=check)
+        except asyncio.TimeoutError:
+            embed.description = "You ran out of time. Try again!"
+            embed.colour = discord.Colour.red()
+            await ctx.send(embed=embed)
+            return
+        if msg.content.upper() in ["Y", "YES"]:  # set the new name for the player
+            unregisterPlayer(ctx)
+            registerPlayer(ctx, leetCodeName)
+            embed.description = f"Your LeetCode account is now registered as `{leetCodeName}`"
+            embed.colour = discord.Colour.green()
+            await ctx.send(embed=embed)
+            return
+        else:
+            embed.description = f"No changes have been made."
+            embed.colour = discord.Colour.purple()
+            await ctx.send(embed=embed)
+            return
+    embed.description = f"Your LeetCode account is already \"{leetCodeName}\"!"
+    embed.colour = discord.Colour.red()
+    await ctx.send(embed=embed)
+
+
+def registerPlayer(ctx, leetCodeName):
+    setUpPlayer(ctx)
+    playerInfo = getPlayer(ctx.author.id)
     playerInfo["LEETCODENAME"] = leetCodeName   # register LeetCode name for player
-    await registeredEmbed(ctx, leetCodeName)
+    setPlayer(ctx.author.id, playerInfo)
 
-    with open(f"./players/{playerID}.json", "w") as writeFile:  # save player info
-        json.dump(playerInfo, writeFile)
 
-def getPlayerLCName(ctx):
-    playerID = str(ctx.author.id)
-    with open(f"./players/{playerID}.json") as readFile:
-        playerInfo = json.load(readFile)
-    
-    return playerInfo["LEETCODENAME"]
+def unregisterPlayer(ctx):
+    os.remove(f"players/{ctx.author.id}.json")
+
+
+def checkIfAccExists(leetCodeName):
+    url = "https://leetcode-stats-api.herokuapp.com/" + leetCodeName
+    leetCodeData = json.loads(urlopen(url).read())
+    if leetCodeData["status"] != "success":
+        return False
+    return True
