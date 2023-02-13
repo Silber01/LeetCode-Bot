0dac68f233ca0f11c3081f6831ce989e22012f1d
Daniel Slade <dangslade@gmail.com>
2023-02-13 01:37:20 -0800

    fixes formatting and content mistakes in setlist data

--- makeProblemSets.py
+++ makeProblemSets.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import copy
 from getQuestionFromLC import getAllQuestions
 
-setlist= "blind75"
+setlist = "blind75"
 problemNames = open(f"problemSetInfo/{setlist}problems.txt", "r")
 problemTopics = open(f"problemSetInfo/{setlist}Topics.txt", "r")
 probList = problemNames.read().split("\n")
@@ -22,7 +22,7 @@ for b in topicList:
     i += 1
 
 problemInfo = defaultdict(lambda: [])
-setlistProblems = {"IDs": []}
+setlistProblems = {"IDs": {}}
 probsFound = set()
 allQuestions = getAllQuestions()
 for problem in problemTopic:  # purposefully inefficient to preserve order, not really that important to optimize
@@ -34,7 +34,7 @@ for problem in problemTopic:  # purposefully inefficient to preserve order, not 
             newProblem["TITLE"] = q["title"]
             newProblem["URL"] = "https://leetcode.com/problems/" + q["titleSlug"]
             newProblem["DIFFICULTY"] = q["difficulty"]
-            setlistProblems["IDs"].append({i + 1: copy.deepcopy(newProblem)})
+            setlistProblems["IDs"][i+1] = copy.deepcopy(newProblem)
             newProblem["ID"] = i + 1
 
             topic = problemTopic[q["title"].lower()]
@@ -50,16 +50,20 @@ if setlist == "neetcode150":
     with open(f"problemSetInfo/setlistProblemIDs.json", "w") as writeFile:
         json.dump(setlistProblems, writeFile)
 elif setlist == "blind75":
-    print(probList)
-    with open("problemSetInfo/setlistProblemIDs.json", "r") as readFile:
-        problemIDs = json.load(readFile)
-    for p in problemIDs["IDs"]:
-        p = p[tuple(p.keys())[0]]
-        print(p["TITLE"])
-        if p["TITLE"] in probList:
-            p["INBLIND75"] = True
+    i = 0
+    inBlind75 = set(setlistProblems["IDs"].keys())
+    print(inBlind75)
+    with open(f"problemSetInfo/setlistProblemIDs.json", "r") as readFile:
+        setlistProblems = json.load(readFile)
+    for s in setlistProblems["IDs"]:
+        print(s)
+        if int(s) in inBlind75:
+            setlistProblems["IDs"][s]["INBLIND75"] = True
+            i += 1
         else:
-            p["INBLIND75"] = False
+            setlistProblems["IDs"][s]["INBLIND75"] = False
     with open(f"problemSetInfo/setlistProblemIDs.json", "w") as writeFile:
-        json.dump(problemIDs, writeFile)
+        json.dump(setlistProblems, writeFile)
+    print(i)
+
 
